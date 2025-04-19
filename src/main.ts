172071df0b0bb936a3c5b726977bf4dc62ee3e09
apptcion <apptcion@gmail.com>
2025-04-19 10:16:31 +0900

    change port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(49154);
+  await app.listen(3000);
 }
 bootstrap();
